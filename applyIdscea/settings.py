59b770b2e2ed3115b0acc2743d1bd0a8f4788e17
Carlos Munoz <carlos7ags@gmail.com>
2022-04-08 18:30:01 -0700

    update static field

--- applyIdscea/settings.py
+++ applyIdscea/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/var/www/fomentoatalentos.com/static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
