bd3c87392b94ecf7424f1862163ac95631ec2355
Carlos Munoz <carlos7ags@gmail.com>
2022-06-11 13:28:49 -0700

    add apoyo form

--- applications/forms.py
+++ applications/forms.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-from crispy_forms.bootstrap import FormActions
+from crispy_forms.bootstrap import FormActions, PrependedAppendedText, PrependedText
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Column, Field, Layout, Row, Submit
+from crispy_forms.layout import Column, Field, Layout, Row, Submit, HTML, Div
 from django import forms
 from django.forms import ModelForm
 
@@ -57,6 +57,7 @@ class ApplicationApoyoForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
+            HTML("<h5>Información del programa de estudios</h5>"),
             Row(
                 Column("date_start", css_class="form-group col-md-4 mb-1"),
                 Column("date_end", css_class="form-group col-md-4 mb-1"),
@@ -66,10 +67,170 @@ class ApplicationApoyoForm(ModelForm):
                 Column(Field("program_name"), css_class="form-group col-md-8 mb-1"),
                 css_class="form-row",
             ),
+            Row(
+                Column(Field("name"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("description"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("objective"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("credits"), css_class="form-group col-md-4 mb-1"),
+            ),
+            Row(
+                Column(Field("duration_hours"), css_class="form-group col-md-4 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("language"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("area"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("impact"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("institution"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("location"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("materias"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("research_topic"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("program_requirements"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            HTML("<h5>Apoyo solicitado</h5>"),
+            HTML("<p>El monto total del apoyo solicitado no puede exceder $20,000.00 al extranjero y $10,000.00 al interior del país.</p>"),
+            Div(
+                HTML('<table class="table table-sm w-auto"'),
+                HTML('<thead class="thead-dark">'),
+                HTML("<tr>"),
+                HTML('<th scope="col">Concepto de apoyo</th>'),
+                HTML('<th scope="col">Monto solicitado</th>'),
+                HTML('<th scope="col">Costo total</th>'),
+                HTML("</tr>"),
+                HTML("</thead>"),
+
+                HTML("<tbody>"),
+
+                HTML("<tr>"),
+                HTML('<th scope="row">Movilidad</th>'),
+                HTML("<td>"),
+                PrependedText("total_movilidad", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("<td>"),
+                PrependedText("requested_movilidad", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("</tr>"),
+
+                HTML("<tr>"),
+                HTML('<th scope="row">Investigación</th>'),
+                HTML("<td>"),
+                PrependedText("total_investigacion", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("<td>"),
+                PrependedText("requested_investigacion", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("</tr>"),
+
+                HTML("<tr>"),
+                HTML('<th scope="row">Inscripción</th>'),
+                HTML("<td>"),
+                PrependedText("total_inscripcion", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("<td>"),
+                PrependedText("requested_inscripcion", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("</tr>"),
+
+                HTML("<tr>"),
+                HTML('<th scope="row">Viáticos</th>'),
+                HTML("<td>"),
+                PrependedText("total_viaticos", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("<td>"),
+                PrependedText("requested_viaticos", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("</tr>"),
+
+                HTML("<tr>"),
+                HTML('<th scope="row">Otros</th>'),
+                HTML("<td>"),
+                PrependedText("total_otros", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("<td>"),
+                PrependedText("requested_otros", "$", css_class="form-group text-right col-md-6"),
+                HTML("</td>"),
+                HTML("</tr>"),
+
+                HTML("</tbody>"),
+                HTML("</table>"),
+                css_class="form-row",
+            ),
+
+            Row(
+                Column(Field("personal_statement"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("suitability"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("future_plans"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("importance"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                HTML("<p>Complete el siguiente texto donde exprese los motivos personales por los cuales usted está solicitando el apoyo del programa de fomento a talentos en la Modalidad de Apoyos Directos y Estímulos correspondiente, considerando los puntos que a continuación se describen dentro de la redacción general.</p>"),
+            ),
+            Row(
+                Column(Field("statement_of_purpose"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("program_relation_cti"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+            Row(
+                Column(Field("program_relation_state"), css_class="form-group col-md-8 mb-1"),
+                css_class="form-row",
+            ),
+
             FormActions(
                 Submit("save", "Guardar"),
             ),
         )
+
+
+
+
+
+
+
+
 
     class Meta:
         model = ApplicationContentApoyo
--- applications/models.py
+++ applications/models.py
@@ -35,57 +35,6 @@ class Award(models.Model):
         return "%s - %s" % (self.program, self.username)
 
 
-"""
-class DirectAwardApplicationForm(models.Model):
-    AWARD_STATUS = ((0, "Rechazado"), (1, "Aprobado"))
-
-    username = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        verbose_name="Solicitante",
-    )
-    program = models.ForeignKey(
-        to=Program,
-        on_delete=models.CASCADE,
-        verbose_name="Programa",
-    )
-
-    start_date = models.DateField("Fecha de Inicio de estudios, especialidad, posgrado o actividad académica para la cuál requiere el apoyo.")
-    end_date = models.DateField("Fecha de término de estudios, especialidad, Posgrado o actividad académica para la cuál requiere el apoyo.", null=True, blank=True)
-    program_name = models.CharField("Nombre de los estudios o actividad que realizará y para la cuál pide el apoyo.", max_length=100)
-    program_objective = models.TextField("Objetivo de los estudios o actividad que realizará y para la cuál pide el apoyo.")
-    program_area = models.CharField("Área o perfil de los estudios o actividad que realizará y para la cuál pide el apoyo.", max_length=100)
-    program_institution = models.CharField("Institución donde realizará sus estudios o actividad.", max_length=100)
-    program_location = models.CharField("Ciudad o país donde realizará sus estudios o actividad.", max_length=100)
-
-    current_institution = models.CharField("Institución o empresa donde estudia o trabaja actualmente.", max_length=100)
-    last_program = models.CharField("Último grado académico o módulo cursado.", max_length=100)
-    last_grade = models.CharField("Promedio último obtenido / equivalencia (con documento oficial institucional si es correspondiente al extranjero, solo aplica para modalidad por convocatoria)", max_length=100)
-    other = models.TextField("Otros.")
-
-    total_movilidad = models.IntegerField("")
-    requested_movilidad = models.IntegerField("")
-    total_investigacion = models.IntegerField("")
-    requested_investigacion = models.IntegerField("")
-    total_inscripcion = models.IntegerField("")
-    requested_inscripcion = models.IntegerField("")
-    total_viaicos = models.IntegerField("")
-    requested_viaticos = models.IntegerField("")
-    total_otros = models.IntegerField("")
-    requested_otros = models.IntegerField("")
-
-    personal_statement = models.TextField("Objetivo personal que lo impulsó para seguir estudiando.")
-    suitability = models.TextField("Diga, ¿por qué es pertinente el apoyar su formación en la modalidad pretendida?")
-    future_plans = models.TextField("Planes de trabajo después de terminar sus estudios.")
-    importance = models.TextField("Fundamente las ventajas de esta institución y programa sobre la oferta educativa en Aguascalientes.")
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return "%s - %s" % (self.program, self.username)
-"""
-
-
 class Application(models.Model):
     APPLICATION_STAGES = (
         (0, "Pendiente"),
@@ -152,6 +101,15 @@ class ApplicationContentConvocatoria(models.Model):
 
 
 class ApplicationContentApoyo(models.Model):
+
+    LANGUAGES = (
+        ("spanish", "Español"),
+        ("english", "Inglés"),
+        ("portuguese", "Portugués"),
+        ("french", "Francés"),
+        ("french", "Otro"),
+    )
+
     id = models.OneToOneField(
         Application,
         on_delete=models.CASCADE,
@@ -159,6 +117,48 @@ class ApplicationContentApoyo(models.Model):
     )
     date_start = models.DateField(verbose_name="Fecha de inicio del programa")
     date_end = models.DateField(verbose_name="Fecha de término del programa")
-    program_name = models.CharField(
+    name = models.CharField(
         verbose_name="Nombre del programa de estudios", max_length=120
     )
+    description = models.TextField(
+        verbose_name="Descripción del programa, tema de estudio o actividad académica que pretende realizar"
+    )
+    objective = models.TextField("Objetivo de los estudios o actividad que realizará y para la cuál pide el apoyo")
+    credits = models.IntegerField("Número de asignaturas o créditos del programa")
+    duration_hours = models.IntegerField("Duración del programa de estudios (horas)")
+    language = models.CharField("Idioma en que se imparte el programa",
+                                choices=LANGUAGES,
+                                max_length=50,
+                                )
+    area = models.CharField("Área del programa", max_length=100)
+    impact = models.TextField("Impacto y beneficios del programa para el estado")
+    institution = models.CharField("Institución donde realizará sus estudios o actividad", max_length=100)
+    location = models.CharField("Ciudad o país donde realizará sus estudios o actividad", max_length=100)
+    materias = models.TextField("Lista de materias y número de créditos", help_text="Sólo en caso de estar matriculado y cursando un programa de doctorado o maestría. Inserte una materia por renglon con el número de créditos separado con una coma, por ejemplo:<br/>Matemáticas, 4<br/>Español, 4<br/>Ciencias Naturales, 6")
+    research_topic = models.CharField("Línea de investigación", max_length=400, help_text="Solo en caso de estar matriculado y cursando programa de doctorado o maestría.")
+    program_requirements = models.CharField("Link al programa de estudios", max_length=400, help_text="Enlace o link o nombre del documento donde se pueda verificar la información de la estructura curricular y las líneas de investigación. Si la información se encuentra en un documento entregado en su expediente, indicar los números de página en donde se ubica.")
+
+    total_movilidad = models.IntegerField("", default=0)
+    requested_movilidad = models.IntegerField("", default=0)
+    total_investigacion = models.IntegerField("", default=0)
+    requested_investigacion = models.IntegerField("", default=0)
+    total_inscripcion = models.IntegerField("", default=0)
+    requested_inscripcion = models.IntegerField("", default=0)
+    total_viaticos = models.IntegerField("", default=0)
+    requested_viaticos = models.IntegerField("", default=0)
+    total_otros = models.IntegerField("", default=0)
+    requested_otros = models.IntegerField("", default=0)
+
+    personal_statement = models.TextField("Objetivo personal que lo impulsó para seguir estudiando")
+    suitability = models.TextField("Explique, ¿por qué es pertinente el apoyar su formación en la modalidad pretendida?")
+    future_plans = models.TextField("Planes de trabajo después de terminar sus estudios")
+    importance = models.TextField("Fundamente las ventajas de esta institución y programa sobre la oferta educativa en Aguascalientes")
+    statement_of_purpose = models.TextField("Exposición de motivos", default="Me dirijo a usted de la manera más atenta para solicitarle...\n\nObjetivo...\n\nMotivo de solicitud...\n\nBeneficios e impacto que tendrá realizar sus estudios o actividad para la cual requiere el apoyo...\n")
+    program_relation_cti = models.TextField("Señale la relación del proyecto de estudios con los sectores estratégicos y el impacto o beneficios para el Estado")
+    program_relation_state = models.TextField("Argumetación sobre la relación del proyecto o programa de estudios con la ciencia, la tecnología y la innovación")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s - %s - %s" % (self.id, self.id.username, self.id.program)
