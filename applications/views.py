852087fd87a6eed3860a158fbcc9a46d781ae253
Carlos Munoz <carlos7ags@gmail.com>
2022-08-22 20:17:00 -0700

    pdf veresion1

--- applications/views.py
+++ applications/views.py
@@ -228,21 +228,36 @@ def download_application(request, pk):
 def html_to_pdf(template_src: str, context=Dict[str, Any]):
     template = get_template(template_src)
     html = template.render(context)
-    print(html)
     result = BytesIO()
-
     pdf = pisa.pisaDocument(
-        BytesIO(html.encode("utf-8")), result, link_callback=fetch_resources
+        BytesIO(html.encode("utf-8")), result, link_callback=link_callback
     )
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type="application/pdf")
     return None
 
 
-def fetch_resources(uri, rel):
-    path = os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
-    return path
+def link_callback(uri, rel):
+    # use short variable names
+    sUrl = settings.STATIC_URL      # Typically /static/
+    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+    mUrl = settings.MEDIA_URL       # Typically /static/media/
+    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
 
+    # convert URIs to absolute system paths
+    if uri.startswith(mUrl):
+        path = os.path.join(mRoot, uri.replace(mUrl, ""))
+    elif uri.startswith(sUrl):
+        path = os.path.join(sRoot, uri.replace(sUrl, ""))
+    else:
+        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+            raise Exception(
+                'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+    return path
 
 def upload_final_proof(request, pk):
     award = Award.objects.get(id=pk)
--- applyIdscea/settings.py
+++ applyIdscea/settings.py
@@ -165,7 +165,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'fomentoatalentos@gmail.com'
-EMAIL_HOST_PASSWORD = 'lzbhlvkwplghknmp'
+EMAIL_HOST_PASSWORD = 'tdktwlruajehtegi'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
