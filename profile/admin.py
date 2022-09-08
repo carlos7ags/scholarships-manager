6c8ab1ff9b662f5b7116c0f2eddb28c760057d24
Carlos Munoz <carlos7ags@gmail.com>
2022-09-07 21:35:37 -0500

    add export admin bank

--- profile/admin.py
+++ profile/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 from .models import *
 
@@ -7,3 +9,14 @@ admin.site.register(Bank)
 admin.site.register(Contact)
 admin.site.register(Address)
 admin.site.register(EmergencyContact)
+
+
+class BankResource(resources.ModelResource):
+    class Meta:
+        model = Bank
+
+
+class BankAdmin(ImportExportModelAdmin):
+    resource_class = BankResource
+
+admin.site.register(Bank, BankAdmin)
