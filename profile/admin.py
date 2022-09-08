7c5c4e2d8adadbedd5fc1dc4f9faf4febc76b7dc
Carlos Munoz <carlos7ags@gmail.com>
2022-09-07 21:36:59 -0500

    fix

--- profile/admin.py
+++ profile/admin.py
@@ -5,7 +5,6 @@ from import_export import resources
 from .models import *
 
 admin.site.register(Profile)
-admin.site.register(Bank)
 admin.site.register(Contact)
 admin.site.register(Address)
 admin.site.register(EmergencyContact)
